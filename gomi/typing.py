985def94f7bac22d9237fd224ced6d848a617bc4
Ren Tatsumoto <tatsu@autistici.org>
2025-04-30 13:58:18 +0300

    format

--- gomi/typing.py
+++ gomi/typing.py
@@ -7,9 +7,10 @@ class GomiOnDiskModelDict(typing.TypedDict):
     """
     Model JSON stored on disk.
     """
+
     modelName: str
     inOrderFields: list[str]
-    cardTemplates: list[str] # list of names
+    cardTemplates: list[str]  # list of names
 
 
 class AnkiConnectCardTemplateDict(typing.TypedDict):
@@ -22,5 +23,5 @@ class AnkiConnectModelDict(typing.TypedDict):
     modelName: str
     inOrderFields: list[str]
     css: str
-    isCloze: typing.NotRequired[bool] # isCloze is used when CSS is not specified.
+    isCloze: typing.NotRequired[bool]  # isCloze is used when CSS is not specified.
     cardTemplates: list[AnkiConnectCardTemplateDict]
